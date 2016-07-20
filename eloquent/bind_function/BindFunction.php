81f2fce8ea01e9a2effa32c60060441ab3d94d34
wiselabs <christoffer.luccas@gmail.com>
2016-07-20 11:27:19 -0300

    commiit

--- eloquent/bind_function/BindFunction.php
+++ eloquent/bind_function/BindFunction.php
@@ -9,5 +9,52 @@
 		https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this#Method_binding
 		https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
 	 -->
+	 <script type="text/javascript" charset="utf-8" async defer>
+	 		// criando funcoes usando bind
+	 		'use strict';
+
+	 		var List = function() {
+	 			return Array.prototype.slice.apply(arguments);
+	 		};
+
+	 		var run = function() {
+		 		var list = List(1,2,3);
+	 			console.log(list);
+	 			default_list = List.bind(undefined, [1000, 100, 10]);
+	 			console.log(default_list(1,2,3));
+	 		};
+
+			function Clazz() {}
+
+	 		var run2 = function() {
+
+	 			Clazz.prototype.timer = function(argument) {
+	 				var f1 = this.methodA.bind(this);
+	 				var f2 = this.methodA;
+
+	 				 //window.setTimeout(f2.apply(this), 3000);
+	 				 window.setTimeout(f1, 3000);
+	 				 window.setTimeout(f2, 3000);
+	 			};
+
+	 			Clazz.prototype.methodA = function() {
+	 				console.log(this, 'methodA');
+	 			}
+
+	 			Clazz.prototype.methodB = function(){
+	 				 console.log(this, 'methodB');
+	 			};
+
+
+	 			var clazz = new Clazz();
+	 			clazz.timer();
+	 		};
+
+	 		//run();
+	 		run2();
+	 		//window.onload = run;
+
+
+	 </script>
 </body>
 </html>
