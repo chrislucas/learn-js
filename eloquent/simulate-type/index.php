9bd54f3de86ac272d0fa19846ce73bc2938dbc2a
lucaschris <crhistoffer.luccas@gmail.com>
2016-08-03 18:43:36 -0300

    commit

---
+++ eloquent/simulate-type/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Simulando digitacao</title>
+</head>
+<body>
+	<textarea name="text" id="textarea"></textarea>
+	<script type="text/javascript" charset="utf-8" async defer>
+		/*
+			http://hackertyper.net/
+			
+			https://developer.mozilla.org/pt-BR/docs/Web/API/Element/getElementsByClassName
+			http://stackoverflow.com/questions/6762557/how-to-add-event-listener-for-html-class
+			
+			search
+			queryselectorall vs getelementsbyclassname
+			
+			http://stackoverflow.com/questions/14377590/queryselector-and-queryselectorall-vs-getelementsbyclassname-and-getelementbyid
+
+			http://codepen.io/anon/pen/thnCd
+			http://stackoverflow.com/questions/596481/simulate-javascript-key-events
+		
+			search
+			simulate keypress javascript
+		*/
+
+	</script>
+</body>
+</html>
