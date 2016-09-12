5a7564ff1e2f7ef0e56c66aaaabb00201eba0eb7
wiselabs <christoffer.luccas@gmail.com>
2016-09-12 09:11:22 -0300

    commit

---
+++ eloquent/studyfunc.js
@@ -0,0 +1,20 @@
+// Function expressions vs. Function declarations
+// http://jstherightway.org/pt-br/
+
+/*
+Function expressions vs. Function declarations
+http://kangax.github.io/nfe/
+http://www.permadi.com/tutorial/jsFunc/index.html
+http://stackoverflow.com/questions/11146814/difference-between-assigning-function-to-variable-or-not
+http://stackoverflow.com/questions/336859/javascript-function-declaration-syntax-var-fn-function-vs-function-fn
+*/
+
+// hoisting
+/*
+http://www.adequatelygood.com/JavaScript-Scoping-and-Hoisting.html
+http://adripofjavascript.com/blog/drips/variable-and-function-hoisting
+https://javascriptweblog.wordpress.com/2010/07/06/function-declarations-vs-function-expressions/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions
+http://www.w3schools.com/js/js_hoisting.asp
+http://loopinfinito.com.br/2014/10/29/hoisting-e-escopo-em-javascript/
+*/
